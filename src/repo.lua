ecc5828c963eca6e4555e8f882bb01403ca038e0
b7n <b7n@b7n-PC>
2016-08-08 19:18:52 +0900

    #

--- src/repo.lua
+++ src/repo.lua
@@ -1,4 +1,6 @@
-Reference = {
+N = {}
+
+N.Reference = {
   localRepo = 'bnn/',
   localSystem = '.bnn/',
   remoteDB = '.bnn/remote.json',
@@ -7,7 +9,7 @@ Reference = {
   user_key = 'd7f12b0137d6ba187cef85295ca646b7',
 }
 
-Util = {
+N.Util = {
   join = function(tbl, sep)
     local ret = ''
     for _,v in ipairs(tbl) do
@@ -64,7 +66,7 @@ Util = {
             if deep and copy and type(copy)=='table' then
               clone = src or {}
               -- Never move original objects, clone them
-              target[ name ] = Util.extend( deep, clone, copy )
+              target[ name ] = N.Util.extend( deep, clone, copy )
             -- Don't bring in undefined values
             elseif copy~=nil then
               target[ name ] = copy
@@ -78,23 +80,22 @@ Util = {
   end,
 }
 
-Repo = {}
-Code = {}
-Code.new = function(code)
+N.Code = {}
+N.Code.new = function(code)
   local obj = {
     code = code,
   }
-  return setmetatable(obj, {__index = Code})
+  return setmetatable(obj, {__index = N.Code})
 end
-Code.fromString = function(this, code)
+N.Code.fromString = function(this, code)
   if not this.code then
     this.code = code
   end
   return this
 end
-Code.fromGet = function(this, url, ...)
+N.Code.fromGet = function(this, url, ...)
   if not this.code then
-    local data = Util.join({...}, '&')
+    local data = N.Util.join({...}, '&')
     local response = http.get(url..data)
     if response then
       this.code = response.readAll()
@@ -103,9 +104,9 @@ Code.fromGet = function(this, url, ...)
   end
   return this
 end
-Code.fromPost = function(this, url, ...)
+N.Code.fromPost = function(this, url, ...)
   if not this.code then
-    local data = Util.join({...}, '&')
+    local data = N.Util.join({...}, '&')
     local response = http.post(url, data)
     if response then
       this.code = response.readAll()
@@ -114,7 +115,7 @@ Code.fromPost = function(this, url, ...)
   end
   return this
 end
-Code.fromFile = function(this, path)
+N.Code.fromFile = function(this, path)
   if not this.code then
     if fs.exists(path) then
       local file = fs.open(path, 'r')
@@ -126,28 +127,28 @@ Code.fromFile = function(this, path)
   end
   return this
 end
-Code.bind = function(this, path)
+N.Code.bind = function(this, path)
   this.file = path
   return this
 end
-Code.delete = function(this)
+N.Code.delete = function(this)
   if this.file then
     fs.delete(this.file)
   end
   return this
 end
-Code.getBind = function(this)
+N.Code.getBind = function(this)
   return this.file
 end
-Code.reset = function(this)
+N.Code.reset = function(this)
   this.code = nil
   this.from = nil
   return this
 end
-Code.exists = function(this)
+N.Code.exists = function(this)
   return this.code~=nil
 end
-Code.save = function(this, path)
+N.Code.save = function(this, path)
   if this.code then
     local p = path or this.file
     if p~=this.from then
@@ -168,24 +169,24 @@ Code.save = function(this, path)
   end
   return this
 end
-Code.loadcode = function(this, name)
+N.Code.loadcode = function(this, name)
   if this.code then
     return loadstring(this.code, name or this.file or 'code')
   end
 end
-Code.execute = function(this, ...)
+N.Code.execute = function(this, ...)
   if this.code then
     return this:loadcode()(...)
   end
 end
-Code.invoke = function(this, ...)
+N.Code.invoke = function(this, ...)
   if this.code then
     local func = this:loadcode()
     setfenv(func, getfenv())
     return func(...)
   end
 end
-Code.api = function(this, ...)
+N.Code.api = function(this, ...)
   if this.code then
     local tEnv = {}
     setmetatable( tEnv, { __index = _G } )
@@ -203,49 +204,49 @@ Code.api = function(this, ...)
     end
   end
 end
-Code.get = function(this)
+N.Code.get = function(this)
   return this.code
 end
 
-Lib = {
-  XML = Code.new():fromFile(Reference.localSystem..'xml.lua'):fromGet('http://pastebin.com/raw/cFDg20XW'):save():execute(),
-  JSON = Code.new():fromFile(Reference.localSystem..'json.lua'):fromGet('http://pastebin.com/raw/4YncwwC6'):save():execute(),
+N.Lib = {
+  XML = N.Code.new():fromFile(N.Reference.localSystem..'xml.lua'):fromGet('http://pastebin.com/raw/cFDg20XW'):save():execute(),
+  JSON = N.Code.new():fromFile(N.Reference.localSystem..'json.lua'):fromGet('http://pastebin.com/raw/4YncwwC6'):save():execute(),
 }
 
-Pastebin = {}
-Pastebin.new = function()
+N.Pastebin = {}
+N.Pastebin.new = function()
   local obj = {}
   obj.dbinit = {
-    id = DataBase.PRIMARY_KEY,
+    id = N.DataBase.PRIMARY_KEY,
     'title',
-    name = DataBase.UNIQUE_KEY,
+    name = N.DataBase.UNIQUE_KEY,
     'version',
   }
-  obj.localrepocode = Code.new():fromFile(Reference.localDB)
-  obj.localrepo = IODataBase.new():init(obj.dbinit):fromCode(obj.localrepocode)
-  obj.remoterepocode = Code.new():fromFile(Reference.remoteDB)
-  obj.remoterepo = IODataBase.new():init(obj.dbinit):fromCode(obj.remoterepocode)
-  return setmetatable(obj, {__index = Pastebin})
+  obj.localrepocode = N.Code.new():fromFile(N.Reference.localDB)
+  obj.localrepo = N.IODataBase.new():init(obj.dbinit):fromCode(obj.localrepocode)
+  obj.remoterepocode = N.Code.new():fromFile(N.Reference.remoteDB)
+  obj.remoterepo = N.IODataBase.new():init(obj.dbinit):fromCode(obj.remoterepocode)
+  return setmetatable(obj, {__index = N.Pastebin})
 end
-Pastebin.update = function(this, ...)
+N.Pastebin.update = function(this, ...)
   local updates = {this.remoterepo:get(...)}
   for _,updatel in ipairs(updates) do
     for update,entry in pairs(updatel) do
-      Code.new():fromGet('http://pastebin.com/raw/'..entry.id):save(entry.name)
+      N.Code.new():fromGet('http://pastebin.com/raw/'..entry.id):save(entry.name)
     end
   end
   return this
 end
-Pastebin.fetch = function(this)
+N.Pastebin.fetch = function(this)
   this.remoterepo:clear()
-  local remoteRepoCode = Code.new():fromPost(
+  local remoteRepoCode = N.Code.new():fromPost(
     'http://pastebin.com/api/api_post.php',
     'api_option=list',
-    'api_dev_key='..Reference.dev_key,
-    'api_user_key='..Reference.user_key
+    'api_dev_key='..N.Reference.dev_key,
+    'api_user_key='..N.Reference.user_key
   )
   if remoteRepoCode:exists() then
-    local remoteRepoData = Lib.XML.Parser.new():ParseXmlText(remoteRepoCode:get())
+    local remoteRepoData = N.Lib.XML.Parser.new():ParseXmlText(remoteRepoCode:get())
     for _,v in ipairs(remoteRepoData.paste) do
       local id = v.paste_key:value()
       local title = v.paste_title:value()
@@ -262,51 +263,48 @@ Pastebin.fetch = function(this)
   end
   return this
 end
-Pastebin.add = function(this, add)
+N.Pastebin.add = function(this, add)
   this.localrepo:insert(this.remoterepo:get(add))
   this.localrepo:save(this.localrepocode)
   return this
 end
-Pastebin.merge = function(this)
+N.Pastebin.rm = function(this, rm)
+  local rms = this.remoterepo:get(rm)
+  for rm,entry in pairs(rms) do
+    N.Code.new():bind(N.Reference.localRepo..entry.name):delete()
+  end
+  this.localrepo:rm(rms)
+  this.localrepo:save(this.localrepocode)
+  return this
+end
+N.Pastebin.merge = function(this)
   local remoteentries = this.remoterepo:get()
   local localentries = this.localrepo:get()
   for i,localentry in pairs(localentries) do
     local remoteentry = remoteentries[i]
     if remoteentry.version~=localentry.version then
-      Code.new():fromGet('http://pastebin.com/raw/'..localentry.id):save(Reference.localRepo..localentry.name)
+      N.Code.new():fromGet('http://pastebin.com/raw/'..localentry.id):save(N.Reference.localRepo..localentry.name)
       this.localrepo:insert{remoteentry}
     else
-      Code.new():fromFile(Reference.localRepo..localentry.name):fromGet('http://pastebin.com/raw/'..localentry.id):save()
+      N.Code.new():fromFile(N.Reference.localRepo..localentry.name):fromGet('http://pastebin.com/raw/'..localentry.id):save()
     end
   end
   this.localrepo:save(this.localrepocode)
   return this
 end
--- TODO
---Pastebin.rm = function(this, ...)
---  local rms = {this.localrepo:get(...)}
---  for _,rml in ipairs(rms) do
---    for rm,entry in pairs(rml) do
---      Code.new():bind(entry.name):delete()
---    end
---  end
---  this.localrepo:rm(unpack(rms))
---  this.localrepo:save(this.localrepocode)
---  return this
---end
 
-DataBase = {}
-DataBase.new = function()
+N.DataBase = {}
+N.DataBase.new = function()
   local obj = {}
-  return setmetatable(obj, {__index = DataBase})
+  return setmetatable(obj, {__index = N.DataBase})
 end
-DataBase.fromData = function(this, db)
+N.DataBase.fromData = function(this, db)
   if not this.db then
     this.db = db
   end
   return this
 end
-DataBase.init = function(this, init)
+N.DataBase.init = function(this, init)
   if not this.db then
     this.db = {
       init = init,
@@ -325,25 +323,25 @@ DataBase.init = function(this, init)
         limit = _2
       end
       this.db.data.obj[col] = {}
-      if limit==DataBase.PRIMARY_KEY then
+      if limit==N.DataBase.PRIMARY_KEY then
         if this.db.data.primary==nil then
           this.db.data.primary = col
           this.db.data.xedni = {}
         end
-        this.db.meta[col] = DataBase.DEFAULT
+        this.db.meta[col] = N.DataBase.DEFAULT
       else
-        this.db.meta[col] = limit or DataBase.DEFAULT
+        this.db.meta[col] = limit or N.DataBase.DEFAULT
       end
     end
   end
   return this
 end
-DataBase.clear = function(this)
+N.DataBase.clear = function(this)
   local init = this.db.init
   this:init(init)
   return this
 end
-DataBase._rowspace = function(this, put)
+N.DataBase._rowspace = function(this, put)
   local row
   if this.db.data.primary then
     row = this.db.data.xedni[put[this.db.data.primary]]
@@ -351,7 +349,7 @@ DataBase._rowspace = function(this, put)
   row = row or #this.db.data.index+1
   return row
 end
-DataBase._insertable = function(this, put, row)
+N.DataBase._insertable = function(this, put, row)
   local check = true
   for _col,_dbcol in pairs(this.db.data.obj) do
     if not this.db.meta[_col](_dbcol, row, put[_col]) then
@@ -363,7 +361,7 @@ DataBase._insertable = function(this, put, row)
   end
   return check
 end
-DataBase.insert = function(this, puts)
+N.DataBase.insert = function(this, puts)
   if this.db then
     if puts then
       for _,put in ipairs(puts) do
@@ -386,7 +384,7 @@ DataBase.insert = function(this, puts)
   end
   return this
 end
-DataBase._checkrow = function(this, search, row)
+N.DataBase._checkrow = function(this, search, row)
   local checks = false
   for _,get in ipairs(search) do
     local check = true
@@ -403,7 +401,7 @@ DataBase._checkrow = function(this, search, row)
   end
   return checks
 end
-DataBase._search = function(this, search)
+N.DataBase._search = function(this, search)
   if not (search and #search>0) then
     search = {{}}
   end
@@ -435,7 +433,7 @@ DataBase._search = function(this, search)
   end
   return rows
 end
-DataBase.update = function(this, changes)
+N.DataBase.update = function(this, changes)
   if this.db then
     for _target,_change in pairs(changes) do
       local rows = this:_search(_target)
@@ -455,7 +453,7 @@ DataBase.update = function(this, changes)
   end
   return this
 end
-DataBase.get = function(this, gets)
+N.DataBase.get = function(this, gets)
   if this.db then
     local rows = this:_search(gets)
     local row2 = {}
@@ -469,7 +467,7 @@ DataBase.get = function(this, gets)
     return row2
   end
 end
-DataBase.rm = function(this, rms)
+N.DataBase.rm = function(this, rms)
   if this.db then
     local rows = this:_search(rms)
     for _,_row in ipairs(rows) do
@@ -484,17 +482,17 @@ DataBase.rm = function(this, rms)
   end
   return this
 end
-DataBase.copy = function(this, from, deep)
-  Util.extend(not not deep, this.db, from.db)
+N.DataBase.copy = function(this, from, deep)
+  N.Util.extend(not not deep, this.db, from.db)
 end
-DataBase.link = function(this, from)
+N.DataBase.link = function(this, from)
   this.db = from.db
 end
-DataBase.PRIMARY_KEY = {}
-DataBase.DEFAULT = function(dbcol, row, value)
+N.DataBase.PRIMARY_KEY = {}
+N.DataBase.DEFAULT = function(dbcol, row, value)
   return true
 end
-DataBase.UNIQUE_KEY = function(dbcol, row, value)
+N.DataBase.UNIQUE_KEY = function(dbcol, row, value)
   for _row,_dbrow in pairs(dbcol) do
     if value==_dbrow then
       if row~=_row then
@@ -504,26 +502,26 @@ DataBase.UNIQUE_KEY = function(dbcol, row, value)
   end
   return true
 end
-DataBase.NOT_NULL = function(dbcol, row, value)
+N.DataBase.NOT_NULL = function(dbcol, row, value)
   return value~=nil
 end
 
-IODataBase = {}
-setmetatable(IODataBase, {__index = DataBase})
-IODataBase.new = function()
-  local obj = DataBase.new()
-  return setmetatable(obj, {__index = IODataBase})
+N.IODataBase = {}
+setmetatable(N.IODataBase, {__index = N.DataBase})
+N.IODataBase.new = function()
+  local obj = N.DataBase.new()
+  return setmetatable(obj, {__index = N.IODataBase})
 end
-IODataBase.fromCode = function(this, code)
+N.IODataBase.fromCode = function(this, code)
   if code:exists() then
-    Util.extend(this.db.data, Lib.JSON:decode(code:get()))
+    N.Util.extend(this.db.data, N.Lib.JSON:decode(code:get()))
   end
   return this
 end
-IODataBase.save = function(this, code)
+N.IODataBase.save = function(this, code)
   if this.db then
     if code:getBind() then
-      code:reset():fromString(Lib.JSON:encode(this.db.data)):save()
+      code:reset():fromString(N.Lib.JSON:encode(this.db.data)):save()
     end
   end
   return this
